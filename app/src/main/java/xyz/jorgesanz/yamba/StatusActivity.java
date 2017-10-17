206e81d3c2da7bb6aeb76ef42443482914fca388
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-10-17 16:27:43 +0200

    refactor: Tweet editor as Fragment

--- app/src/main/java/xyz/jorgesanz/yamba/StatusActivity.java
+++ app/src/main/java/xyz/jorgesanz/yamba/StatusActivity.java
@@ -1,99 +1,16 @@
 package xyz.jorgesanz.yamba;
 
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class StatusActivity
-        extends AppCompatActivity
-        implements View.OnClickListener, TextWatcher {
-
-    private static final String TAG = "StatusActivity";
-    EditText statusEditText;
-    Button tweetButton;
-    Twitter twitter;
-    TextView charsCounterTextView;
+        extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
-        statusEditText = (EditText) findViewById(R.id.status_edit_text);
-        tweetButton = (Button) findViewById(R.id.tweet_button);
-        tweetButton.setOnClickListener(this);
-        charsCounterTextView = (TextView) findViewById(R.id.chars_counter_text_view);
-        charsCounterTextView.setText(Integer.toString(140));
-        charsCounterTextView.setTextColor(Color.GREEN);
-        statusEditText.addTextChangedListener(this);
-
-        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-        configurationBuilder.setOAuthConsumerKey(getString(R.string.oauth_consumer_key))
-                .setOAuthConsumerSecret(getString(R.string.oauth_consumer_secret))
-                .setOAuthAccessToken(getString(R.string.oauth_access_token))
-                .setOAuthAccessTokenSecret(getString(R.string.oauth_access_token_secret));
-        TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build());
-        twitter = twitterFactory.getInstance();
     }
 
-    public void onClick(View view) {
-        String status = statusEditText.getText().toString();
-        Log.d(TAG, "onClicked");
-
-        new PostTask().execute(status);
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable statusEditText) {
-        int count = 140 - statusEditText.length();
-        charsCounterTextView.setText(Integer.toString(count));
-        charsCounterTextView.setTextColor(Color.GREEN);
-        if (count < 10) charsCounterTextView.setTextColor(Color.YELLOW);
-        if (count < 0) charsCounterTextView.setTextColor(Color.RED);
-    }
-
-    private final class PostTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                twitter.updateStatus(params[0]);
-                return "Tweet sending success";
-            } catch (TwitterException e) {
-                Log.e(TAG, "Sending failure");
-                e.printStackTrace();
-                return "Tweet sending failure";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Toast.makeText(StatusActivity.this, "Tweet sent", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
