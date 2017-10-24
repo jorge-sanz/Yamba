9865037c16e8717758511cc91269803f7ad60f73
Jorge Sanz <jorsanzp@gmail.com>
2017-10-24 19:35:44 +0200

    feat(settings): Add shared preferences (not functional yet) (#12)

---
+++ app/src/main/java/xyz/jorgesanz/yamba/SettingsActivity.java
@@ -0,0 +1,20 @@
+package xyz.jorgesanz.yamba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            SettingsFragment fragment = new SettingsFragment();
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment, fragment.getClass().getSimpleName())
+                    .commit();
+        }
+    }
+}
